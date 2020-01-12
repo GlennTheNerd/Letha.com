7ed692964e5cf4c63c930a4e804e2472c8767dd9
Glenn Key <glennkey78@live.no>
2020-01-12 15:22:18 +0100

    Added links in the navigation for better navigational logic

--- js/shoppingBag.js
+++ js/shoppingBag.js
@@ -8,7 +8,9 @@ var data = [
         "https://images.unsplash.com/photo-1493054882428-e4c79a9bfa3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1329&q=80",
       id: 1,
       sizes: [37, 38, 39, 40, 41, 43, 44, 45],
-      price: 78.99
+      price: 78.99,
+      color: "Brown",
+      type: "Boots"
     },
     {
       name: "Bashful Brogue",
@@ -18,7 +20,9 @@ var data = [
         "https://images.unsplash.com/photo-1504826023244-4694f7330c73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80",
       id: 2,
       sizes: [37, 38, 39, 40, 41, 43, 44, 45],
-      price: 69.99
+      price: 69.99,
+      color: "Brown",
+      type: "Boots"
     },
     {
       name: "Nice and Easy",
@@ -28,7 +32,9 @@ var data = [
         "https://images.unsplash.com/photo-1534233650908-b471f2350922?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
       id: 3,
       sizes: [41, 39, 37, 42, 43, 44],
-      price: 89.99
+      price: 89.99,
+      color: "Brown",
+      type: "Boots"
     },
     {
       name: "Calm and Casual",
@@ -38,7 +44,9 @@ var data = [
         "https://images.unsplash.com/photo-1548430395-ec39eaf2aa1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80",
       id: 4,
       sizes: [37, 39, 41],
-      price: 79.99
+      price: 79.99,
+      color: "Yellow",
+      type: "Sneakers"
     },
     {
       name: "Urbana",
@@ -48,7 +56,49 @@ var data = [
         "https://images.unsplash.com/photo-1460066122679-741dd6c4ad37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
       id: 5,
       sizes: [41, 39, 37, 42, 44, 46],
-      price: 69.99
+      price: 69.99,
+      color: "Beige",
+      type: "Boots"
+    },
+    {
+      name: "Kal Visuals",
+      description: "Take on these new stylish work boots, and bring nature to the urbans. Feel the comfort and robustness in all kinds of settings.",
+      imageUrl: "https://images.unsplash.com/photo-1542838687-936f417d2f37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80",
+      id: 6,
+      sizes: [39, 40, 41, 42, 43, 45, 46],
+      price: 119.99,
+      color: "Darkened Red",
+      type: "Boots"
+    },
+    {
+      name: "Reza Heydar",
+      description: "Never hesitate again and give your feet what they deserve with these rock solid, army boots. Bring style to the streets and feel the comfort knowing these shoes are both waterproof and crush-proof",
+      imageUrl: "https://images.unsplash.com/photo-1542913269-3230d7bd3a35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+      id: 7,
+      sizes: [40,41,42,43,44,45,46,47],
+      price: 109.99,
+      color: "Beige",
+      type: "Boots"
+    },
+    {
+      name: "Brandon Bean",
+      description: "Perfect for every occasion, Nice and Easy is the perfect all-rounder to ensure you're always looking at the top of your game. It's easy to wear and maintain and will be the perfect accompaniment for after dinner drinks, an interview, or even your next date.",
+      imageUrl: "https://images.unsplash.com/photo-1563128234-dd1a56eed05d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+      id: 8,
+      sizes: [39,40,41,42,43,44,45,46,47],
+      price: 99.99,
+      color: "Brown",
+      type: "Boots"
+    },
+    {
+      name: "Sanoy Saha",
+      description: "Our Sanoy Saha is one of our most recent shoes. It is perfect as formal wear or for office use. Its comfortable sole makes it perfect for all day wear. The Sanoy is made from brown or black leather and is studded using our unique pattern.",
+      imageUrl: "https://images.unsplash.com/photo-1557870187-4304e2c2b357?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+      id: 9,
+      sizes: [39,40,41,42,43,44,45,46,47],
+      price: 89.99,
+      color: "Black",
+      type: "Boots"
     }
   ];
   
@@ -169,12 +219,22 @@ function likeIconChanger(){
 
 //Remove list item
 function removeTrash(id){
-    const retrievedData = JSON.parse(sessionStorage.getItem("ShoppingCart"));
+
+    const retrievedData = JSON.parse(sessionStorage.getItem("ShoppingCart")); //get data from session storage
     console.log(retrievedData)
-    const selectedElement = data.find(obj => obj.id === id);
-    if (retrievedData !== null || retrievedData !== undefined){
-        sessionStorage.removeItem(selectedElement);
-        console.log('')
+
+    const selectedElement = data.find(obj => obj.id === id); //Find the clicked element matching json
+
+    for (let i = 0; i < retrievedData.length; i++) //Loop through index of session storage array
+    console.log(i)
+    
+    if (selectedElement !== null || selectedElement !== undefined){ //Check if the session storage is not empty
+        //Remove the selected Item
+        sessionStorage.removeItem("ShoppingCart" + selectedElement.id);
+        console.log('successfully removed ' + selectedElement.i)
+    } else {
+        //Log back to see if it was not removed
+        console.log('item not removed');
     }
-    console.log('item not removed');
-}
+    //not working?
+};
